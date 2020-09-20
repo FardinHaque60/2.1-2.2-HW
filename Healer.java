978f55d5ef2145104a335d53fadcddc42a1cc32c
FardinHaque60 <67034724+FardinHaque60@users.noreply.github.com>
2020-09-20 16:00:07 -0700

    Add files via upload

---
+++ Healer.java
@@ -0,0 +1,41 @@
+public class Healer {
+
+// decalre instance varibales
+private String name; 
+private int attack; 
+private int level;
+private int health; 
+private int maxHealth;
+private int defense; 
+private int magicAttack;
+private int magicDefense;
+private int agility; 
+private int wisdom;  
+
+    //constructor 
+    public Healer(String n) {
+        name = n; 
+        level = 1; 
+        maxHealth = 16;
+        health = maxHealth; 
+        attack = 4; 
+        defense = 6; 
+        magicAttack = 5; 
+        magicDefense = 10; 
+        agility = 3; 
+        wisdom = 12;
+    }
+
+    //constructor for creating higher level characters
+    public Healer(String n, int lev) {
+        name = n; 
+        level = lev; 
+        maxHealth = 16*lev; 
+        attack = 4*lev; 
+        defense = 6*lev; 
+        magicAttack = 5*lev;
+        magicDefense = 10*lev; 
+        agility = 3*lev; 
+        wisdom = 12*lev; 
+    }
+}
