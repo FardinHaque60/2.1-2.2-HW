383cd673c52eb2465996def038eaeb71090b595e
FardinHaque60 <67034724+FardinHaque60@users.noreply.github.com>
2020-09-20 16:00:46 -0700

    Add files via upload

---
+++ RPGrunnners.java
@@ -0,0 +1,10 @@
+public class RPGrunnners {
+    
+    public static void main(Static[] arg) {
+
+        Warrior c1 = new Warrior("Jim"); 
+        Warrior c2 = new Warrior("Bob");
+        Warrior c3 = c1; 
+        Warrior c4 = new Warrior("Rushil", 10);  
+    }
+}
